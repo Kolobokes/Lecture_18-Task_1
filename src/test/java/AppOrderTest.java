1b69d4b566963ff97b35a721798bbf6c1732f752
Evgeniya Sukhovaya <C226-88-39-D@yandex.ru>
2021-02-19 21:23:45 +0300

    second test

--- src/test/java/AppOrderTest.java
+++ src/test/java/AppOrderTest.java
@@ -17,10 +17,25 @@ public class AppOrderTest {
 
         open("http://localhost:9999");
         SelenideElement form = $("#root");
-        form.$("[data-test-id='name'] .input__control").setValue("Q");
+        form.$("[data-test-id='name'] .input__control").setValue("Иванов Василий Иванович");
         form.$("[data-test-id='phone'] .input__control").setValue("+79094537420");
         form.$(".checkbox__box").click();
         form.$("[type='button']").click();
-        $("[data-test-id='order-success'] span").shouldNotHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void InCorrectNameTest(){
+
+        Configuration.headless = true;
+
+        open("http://localhost:9999");
+        SelenideElement form = $("#root");
+        form.$("[data-test-id='name'] .input__control").setValue("Ivanov Vasiliy Ivanovich");
+        form.$("[data-test-id='phone'] .input__control").setValue("+79094537420");
+        form.$(".checkbox__box").click();
+        form.$("[type='button']").click();
+  //      String text = form.$("[data-test-id='name'] .input__sub").getAttribute("text");
+        $("[data-test-id='name'] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 }
