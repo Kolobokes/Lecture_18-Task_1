e3b2b5d42a60f5933ae88799e107bc4b48cf74b9
Evgeniya Sukhovaya <C226-88-39-D@yandex.ru>
2021-02-19 21:50:13 +0300

    third test

--- src/test/java/AppOrderTest.java
+++ src/test/java/AppOrderTest.java
@@ -3,6 +3,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -35,7 +36,33 @@ public class AppOrderTest {
         form.$("[data-test-id='phone'] .input__control").setValue("+79094537420");
         form.$(".checkbox__box").click();
         form.$("[type='button']").click();
-  //      String text = form.$("[data-test-id='name'] .input__sub").getAttribute("text");
-        $("[data-test-id='name'] .input__sub").shouldHave(exactText("Им и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void InCorrectTelephoneNumberTest(){
+
+        Configuration.headless = true;
+
+        open("http://localhost:9999");
+        SelenideElement form = $("#root");
+        form.$("[data-test-id='name'] .input__control").setValue("Иванов Василий Иванович");
+        form.$("[data-test-id='phone'] .input__control").setValue("79094537420");
+        form.$(".checkbox__box").click();
+        form.$("[type='button']").click();
+        $("[data-test-id='phone'] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void CheckBoxFalseTest(){
+
+        Configuration.headless = true;
+
+        open("http://localhost:9999");
+        SelenideElement form = $("#root");
+        form.$("[data-test-id='name'] .input__control").setValue("Иванов Василий Иванович");
+        form.$("[data-test-id='phone'] .input__control").setValue("+79094537420");
+        form.$("[type='button']").click();
+        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white input_invalid']").should(appear);
     }
 }
